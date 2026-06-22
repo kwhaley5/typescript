e88f550b8125389883e6ab0cf394e4810ac317b8
Kyle <kylewhaley5@gmail.com>
2026-06-22 05:59:51 -0700

    Service Worker

--- Services/src/main.ts
+++ Services/src/main.ts
@@ -1,5 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideServiceWorker('ngsw-worker.js', {
+              enabled: !isDevMode(),
+              registrationStrategy: 'registerWhenStable:30000'
+            })]
+}).catch((err) => console.error(err));
